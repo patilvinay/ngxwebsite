56311261ee8487ff172ae298f543b61fb769cc5d
Vinay <vinay.hpatil@gmail.com>
2020-05-30 04:37:50 +0000

    :sparkles: server script modified

--- src/node/server.js
+++ src/node/server.js
@@ -3,8 +3,11 @@ var path = require("path");
 var serveStatic = require("serve-static");
 var app = express();
 let PORT = 3001;
-const StaticDir = path.join(__dirname, "public")
+
+const StaticDir = '../../www'
+let Path = path.join(__dirname, StaticDir)
+Path = path.resolve(Path)
 console.log("Serving static pages on port:", PORT);
-console.log("Serving Dir :", StaticDir);
-app.use(serveStatic(StaticDir));
+console.log("Serving Dir :", Path);
+app.use(serveStatic(Path));
 app.listen(PORT);
